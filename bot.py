cfdf064a50ea4e4a29b1be78e29fc37f3e634f48
ThinhHong <thinhhong1998@gmail.com>
2024-01-02 15:56:15 -0500

    Added config parser

--- bot.py
+++ bot.py
@@ -1,19 +1,29 @@
+import configparser
+import sys
 import discord
 from discord.ext import commands
 
+config = configparser.ConfigParser()
+try:
+    config.read('configbot.ini')
 
+except Exception as e:
+        print(f'Could not read configuration file {e}')
+        sys.exit()
+
+bot_token = config['botSetting']['botToken']
+channel_id = int(config['botSetting']['channelId'])
 
 bot_description = "This bot is designed to help students study in groups"
 bot_intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='!', description=bot_description, intents=bot_intents)
 
+
 @bot.event
 async def on_ready():
     message = "Lets start studying!"
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print(message)
-    
     channel = bot.get_channel(channel_id)
     await channel.send(message)
 
